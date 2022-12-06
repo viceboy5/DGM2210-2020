b689b204bc30c427d9470a7d752dd0bb07180b77
Hank Weiss <10811528@my.uvu.edu>
2022-12-05 18:48:37 -0700

    finished nurb color change
    Changes color of any selected object/shapes in the scene.  Color determined with input between 1-32.  Error message thrown if outside those bounds

--- PythonProjects/NurbColorChange/NurbColorChange.py
+++ PythonProjects/NurbColorChange/NurbColorChange.py
@@ -1,13 +1,15 @@
 import maya.cmds as cmds
+def ChangeOutlineColor(color):
 
-def ChangeOutlineColor():
-
-    sel = cmds.ls(sl=True)[0]
-    shape = cmds.listRelatives(sel)
-
-    cmds.setAttr(shape[0] + ".overrideEnabled", 1)
-    cmds.setAttr(shape[0] + ".overrideColor", 15)
+    try:
+        sels = cmds.ls(sl=True)
+        for sel in sels:
+            shape = cmds.listRelatives(sel)
+            cmds.setAttr(shape[0] + ".overrideEnabled", 1)
+            cmds.setAttr(shape[0] + ".overrideColor", color)
+    except:
+        print("Input must be between 1 and 32 to assign new Index Color")
 
 #"color" must be between either 1 or 0 and 32
-ChangeOutlineColor()
+ChangeOutlineColor(31)
 
