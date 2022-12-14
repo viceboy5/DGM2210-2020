022507a605d22f2186ee5e0bb17b5f3986502c1a
Hank Weiss <10811528@my.uvu.edu>
2022-12-13 21:45:55 -0700

    working sequential renamer

--- PythonProjects/Tools/SequentialRenamer.py
+++ PythonProjects/Tools/SequentialRenamer.py
@@ -1,8 +1,27 @@
-txt = "Leg_##_Int_####"
+import maya.cmds as cmds
+
+txt = "Leg_##_Int"
 
 count = txt.count("#")
+nums = "#" * count
 
-#if count > 0:
+x = txt.find(nums)
+if x == -1:
+    print("# should only be used in between the Name and NodeType.  Ensure all arguments are named appropriately")
+else:
+    parts = txt.partition(nums)
+    print(parts)
+    print(parts[1])
 
-x = txt.find("#" * count)
-print(x)
+    sels = cmds.ls(sl=True)
+    for sel in sels:
+        i = sels.index(sel)
+        print(i)
+        nums = nums.replace(nums, str(i))
+        nums = nums.zfill(count)
+
+        cmds.rename([sel], parts[0] + nums + parts[2])
+        print(sel)
+
+
+##def RenameSequentially(Leg_###_Jnt)
