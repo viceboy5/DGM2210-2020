5670c1d29cb58d1b57328bac89cf7b2ecaefef47
Hank Weiss <10811528@my.uvu.edu>
2022-12-13 21:59:36 -0700

    removed print statements and cleaned up script

--- PythonProjects/Tools/SequentialRenamer.py
+++ PythonProjects/Tools/SequentialRenamer.py
@@ -1,27 +1,29 @@
 import maya.cmds as cmds
 
-txt = "Leg_##_Int"
-
-count = txt.count("#")
-nums = "#" * count
-
-x = txt.find(nums)
-if x == -1:
-    print("# should only be used in between the Name and NodeType.  Ensure all arguments are named appropriately")
-else:
-    parts = txt.partition(nums)
-    print(parts)
-    print(parts[1])
-
-    sels = cmds.ls(sl=True)
-    for sel in sels:
-        i = sels.index(sel)
-        print(i)
-        nums = nums.replace(nums, str(i))
-        nums = nums.zfill(count)
-
-        cmds.rename([sel], parts[0] + nums + parts[2])
-        print(sel)
 
 
-##def RenameSequentially(Leg_###_Jnt)
+def RenameSequentially(txt):
+
+    count = txt.count("#")
+    nums = "#" * count
+
+    x = txt.find(nums)
+    if x == -1:
+        print("# should only be used in between the Name and NodeType.  Ensure all arguments are named appropriately")
+    else:
+        parts = txt.partition(nums)
+
+        sels = cmds.ls(sl=True)
+        for sel in sels:
+            i = sels.index(sel)
+            newNum = i + 1
+            nums = nums.replace(nums, str(newNum))
+            nums = nums.zfill(count)
+
+            cmds.rename([sel], parts[0] + nums + parts[2])
+
+
+RenameSequentially("Leg_#######_Jnt")
+
+
+
